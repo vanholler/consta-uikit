bd5836f3d6e8e447a7243c18fad2341348606d76
gizeasy <gizeasy@gmail.com>
2021-06-24 17:33:37 +0300

    docs(Combobox): added tests
    issue -  #1264

--- src/components/UserSelect/helpers.ts
+++ src/components/UserSelect/helpers.ts
@@ -132,3 +132,11 @@ export function withDefaultGetters<
     getItemAvatarUrl: props.getItemAvatarUrl || defaultGetItemAvatarUrl,
   };
 }
+
+export const searchCompare = (searchValue: string, compare?: string): boolean => {
+  if (!compare) {
+    return false;
+  }
+
+  return compare.toLocaleLowerCase().indexOf(searchValue.toLocaleLowerCase()) !== -1;
+};
